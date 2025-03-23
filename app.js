8106e98a8ac4b42a87fc8a269fa1772c22e4b6ed
Wing <eamonn.kearns@so-4pt.net>
2025-03-23 02:01:40 +0000

    Moved router down to just above its app.use()

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 import http from 'http';
 import express from 'express';
+
+
 import path from 'path';
+
 import { fileURLToPath } from 'url';
-
-// routers
-import indexRouter from './routes/index.js';
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const port = 3000;
-
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// routers
+import indexRouter from './routes/index.js';
 
 app.use('/', indexRouter);
 
